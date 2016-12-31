d5258b6e82ce54de02b9297fbcc0f0aa930b6092
chenxilin01 <chenxilinsidney@gmail.com>
2017-01-01 03:55:28 +0800

    hard level choice css and xml done

--- pages/game/programmer.js
+++ pages/game/programmer.js
@@ -2,7 +2,10 @@
 Page({
   data:{
     test_value:[0,0,0,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192],
-    restart_text:"restart"
+    restart_text:"restart",
+    hard_level:[1,2,3,4,5],
+    level_default_idx: [2],
+    level_prompt:"hard"
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
