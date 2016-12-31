37d6f82b2839ddee3ecae375eaa08e0c9c39ea0a
chenxilin01 <chenxilinsidney@gmail.com>
2017-01-01 00:16:14 +0800

    2048 block css and xml

--- pages/game/programmer.js
+++ pages/game/programmer.js
@@ -1,6 +1,8 @@
 // pages/game/programmer.js
 Page({
-  data:{},
+  data:{
+    test_value:[2,4,8,16,32,64,128,256,512,1024,2048,4096,8192]
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
