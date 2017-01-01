eed683a2231258d312063cb670e82ee7d3162ba7
chenxilin01 <chenxilinsidney@gmail.com>
2017-01-01 18:19:51 +0800

    game 2048 merge function implementation

--- utils/game2048.js
+++ utils/game2048.js
@@ -9,13 +9,14 @@ function printAuthor(){
 
 // const value
 // game array length
+const GAME_1DARRAY_LENGTH = 4;
 const GAME_ARRAY_LENGTH = 16;
 // game array value
 const GAME_AVAILABLE_VALUE = [0,2,4,8,16,32,64,128,512,1024,4096,9192];
 // game random value to create number in array
 const GAME_PRIMITIVE_VALUE = [2,4];
 // game move direction
-const GAME_MOVE_DIRECTION = ["left", "right", "top", "bottom"];
+const GAME_MOVE_DIRECTION = ["moveLeft", "moveRight", "moveTop", "moveButtom"];
 // game status
 const GAME_STATUS = ["playing", "end"];
 // game award
@@ -35,6 +36,12 @@ function setGameArray(array) {
         return false;
     gameArray = array.slice();
     console.log("setGameArray gameArray value: ", gameArray);
+    return true;
+}
+// game reset game array
+function resetGameArray() {
+    gameArray.fill(0);
+    console.log("resetGameArray gameArray value: ", gameArray);
     return true;
 }
 // game get game array
@@ -96,7 +103,7 @@ function checkGameStaus() {
 }
 // game array ilegal detect
 function checkArrayLegal(array) {
-    if (Array.isArray(array) == false ||
+    if (!Array.isArray(array) ||
         array.length != GAME_ARRAY_LENGTH)
         return false;
     var existInvalidValue = array.some(function (element, index, array) {
@@ -109,13 +116,103 @@ function checkArrayLegal(array) {
         return false;
     return true;
 }
-// merge game line move
-function mergeGameLine(list, direction) {
-
+// merge game 1D-array move, change 1D-array to next state, merge from array tail to head
+function merge1DArray(array) {
+    // console.log("merge1DArray", array);
+    var newArray = [];
+    if (!Array.isArray(array))
+        return newArray;
+    var length = array.length;
+    if (length == 0)
+        return newArray;
+    // push element
+    var arrayIndex = 0;
+    while (arrayIndex < length - 1) {
+        if (array[arrayIndex] == 0) {
+            arrayIndex++;
+        } else if (array[arrayIndex] == array[arrayIndex + 1]) {
+            newArray.push(2 * array[arrayIndex]);
+            arrayIndex += 2;
+        } else {
+            newArray.push(array[arrayIndex]);
+            arrayIndex++;
+        }
+    }
+    // last element
+    while (arrayIndex < length) {
+        newArray.push(array[arrayIndex]);
+        arrayIndex++;
+    }
+    // filled with zero
+    var zeroCount = length - newArray.length;
+    while (zeroCount--) {
+        newArray.push(0);
+    }
+    // console.log("merge1DArray", newArray);
+    return newArray;
+}
+// merge game 1D-array move, change 1D-array to next state
+function mergeGameLine(array, direction) {
+    if (!Array.isArray(array) || GAME_MOVE_DIRECTION.indexOf(direction) == -1) {
+        return [];
+    }
+    if (direction == "moveLeft" || direction == "moveTop") {
+        return merge1DArray(array);
+    } else {// if (direction == "moveRight" || direction == "moveButtom")
+        var reverseArray = array.slice();
+        reverseArray.reverse();
+        return ((merge1DArray(reverseArray)).reverse());
+    }
 }
 // merge game array when move by direction
 function mergeGameArray(direction) {
-
+    if (GAME_MOVE_DIRECTION.indexOf(direction) == -1) {
+        return false;
+    }
+    var newArray = [];
+    if (direction == "moveRight" || direction == "moveLeft") {
+        for (var count = 0; count < GAME_1DARRAY_LENGTH; count++) {
+            var array = gameArray.filter(function(value, index, array) {
+                return index >= count * GAME_1DARRAY_LENGTH &&
+                    index < (count + 1) * GAME_1DARRAY_LENGTH;
+            });
+            var mergearray = mergeGameLine(array, direction);
+            newArray.push(...mergearray);
+            console.log("old array list", array);
+            console.log("new array list", mergeGameLine(array, direction));
+        }
+    } else { //if (direction == "moveTop" || direction == "moveButtom") {
+        var tempArray = [];
+        for (var count = 0; count < GAME_1DARRAY_LENGTH; count++) {
+            var array = gameArray.filter(function(value, index, array) {
+                return index % GAME_1DARRAY_LENGTH == count;
+            });
+            var mergearray = mergeGameLine(array, direction);
+            tempArray.push(...mergearray);
+            console.log("old array list", array);
+        }
+        for (var count = 0; count < GAME_1DARRAY_LENGTH; count++) {
+            var array = tempArray.filter(function(value, index, array) {
+                return index % GAME_1DARRAY_LENGTH == count;
+            });
+            newArray.push(...array);
+            console.log("new array list", array);
+        }
+    }
+    gameArray = newArray.slice();
+    return true;
+}
+// diplay for debug
+function displaygameArray() {
+    console.log("2048 array display start");
+    for (var count = 0; count < GAME_1DARRAY_LENGTH; count++) {
+        var array = gameArray.filter(function(value, index, array) {
+            return index >= count * GAME_1DARRAY_LENGTH &&
+                index < (count + 1) * GAME_1DARRAY_LENGTH;
+        });
+        console.log("array list", array);
+    }
+    console.log("2048 array display end");
 }
 // unit test
 function testModule() {
@@ -131,6 +228,34 @@ function testModule() {
     console.log("checkArrayLegal: ", checkArrayLegal());
     getGameScore();
     generateGameValue();
+    // test merge function
+    merge1DArray([4,3,2,5,4,6,6,7,8,1,5,2,3,3]);
+    var testarray = [0,3,3,4,5,3,3,6,6,6,7,0,0,0];
+    var outarray = mergeGameLine(testarray, "right");
+    console.log("mergeGameLine: ", outarray);
+    console.log("test moveRight:");
+    setGameArray([0,0,2,2,4,4,8,8,16,32,32,0,16,0,0,0]);
+    displaygameArray();
+    mergeGameArray("moveRight");
+    displaygameArray();
+
+    console.log("test moveLeft:");
+    setGameArray([0,0,2,2,4,4,8,8,16,32,32,0,16,0,0,0]);
+    displaygameArray();
+    mergeGameArray("moveLeft");
+    displaygameArray();
+
+    console.log("test moveTop:");
+    setGameArray([0,0,2,2,4,4,8,8,16,32,32,0,16,0,0,0]);
+    displaygameArray();
+    mergeGameArray("moveTop");
+    displaygameArray();
+
+    console.log("test moveButtom:");
+    setGameArray([0,0,2,2,4,4,8,8,16,32,32,0,16,0,0,0]);
+    displaygameArray();
+    mergeGameArray("moveButtom");
+    displaygameArray();
 }
 module.exports = {
     printAuthor: printAuthor,
