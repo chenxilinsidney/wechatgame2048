e68901c19c9b180facbe6f66bd0769e20ddd5a06
chenxilin01 <chenxilinsidney@gmail.com>
2017-01-01 12:46:44 +0800

    add event and 2048 module

--- pages/game/programmer.js
+++ pages/game/programmer.js
@@ -1,4 +1,5 @@
 // pages/game/programmer.js
+var game2048 = require('../../utils/game2048.js')
 Page({
   data:{
     test_value:[0,0,0,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192],
@@ -15,11 +16,21 @@ Page({
   },
   onShow:function(){
     // 页面显示
+    console.log("on show");
+    game2048.printAuthor();
   },
   onHide:function(){
     // 页面隐藏
   },
   onUnload:function(){
     // 页面关闭
+  },
+  handlerestart:function(event){
+    // 游戏重新开始
+    console.log(event);
+  },
+  handlemove:function(event){
+    // 游戏网格界面触摸
+    console.log(event);
   }
 })
---
+++ utils/game2048.js
@@ -0,0 +1,12 @@
+
+var author = 'chenxilinsdney';
+var email = 'chenxilinsidney@gmail.com';
+
+function printAuthor(){
+    console.log("game2048 author: " + author);
+    console.log("game2048 author email: " + email);
+}
+
+module.exports = {
+    printAuthor: printAuthor
+}
